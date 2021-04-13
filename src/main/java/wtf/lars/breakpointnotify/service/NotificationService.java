d40d6054a763cae532ffafffb0fe843f9d77809a
Lars Lauger <lars.lauger@netlogix.de>
2021-04-13 11:04:47 +0200

    FEATURE: Update to intelliJ 211.*

--- src/main/java/wtf/lars/breakpointnotify/service/NotificationService.java
+++ src/main/java/wtf/lars/breakpointnotify/service/NotificationService.java
@@ -5,6 +5,7 @@ import com.intellij.notification.NotificationDisplayType;
 import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
 
 public class NotificationService {
 
@@ -14,7 +15,7 @@ public class NotificationService {
             true
     );
 
-    public void sendNotification(Project activeProject, Project originProject) {
+    public void sendNotification(Project activeProject, @NotNull Project originProject) {
         final String content = String.format("Breakpoint reached in other project \"%s\".", originProject.getName());
         final Notification notification = NOTIFICATION_GROUP.createNotification(content, NotificationType.INFORMATION);
         notification.notify(activeProject);
